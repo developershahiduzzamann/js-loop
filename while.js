249a45069f1553c8dd5746e3e4ad850b953f7784
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-08-18 05:08:06 +0600

    addad the whele loop

---
+++ while.js
@@ -0,0 +1,12 @@
+const number = 7;
+if(number < 7){
+    console.log('choto ekta number paichi');
+}
+console.log('somthing new is cooking');
+
+
+let num = 0;
+while(num< 10){
+    console.log('hello', num);
+    num = num + 1;
+}
