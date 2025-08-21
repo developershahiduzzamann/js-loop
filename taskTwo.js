7f5333c830fa8e6a14da4eb8623981bb7380da0e
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-08-21 10:14:53 +0600

    add the for loop odd and even number code

---
+++ taskTwo.js
@@ -0,0 +1,11 @@
+// find a odd number
+
+for(i = 61; i <= 100; i = i + 2) {
+    console.log('Odd Number:', i);
+}
+
+// find a even number
+
+for(i = 78; i <= 98; i = i + 2) {
+    console.log('Even Number:', i);
+}
