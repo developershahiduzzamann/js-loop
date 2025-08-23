9f74ea6603d490d3050bd3917376692a8e8dca24
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-08-24 01:12:18 +0600

    add the break

--- break.js
+++ break.js
@@ -1,6 +1,15 @@
-for(let i = 0; i<= 50; i++) {
-    console.log(i);
-    if(i >= 10) {
-        break;
+// for(let i = 0; i<= 50; i++) {
+//     console.log(i);
+//     if(i >= 10) {
+//         break;
+//     }
+// }
+
+let i = 50;
+while(i >= 1){
+    if(i <= 25 ){
+        break
     }
+    i--
+    console.log(i);
 }
